40a36a0ab8beabe1d0d8ab371da7b905ba387872
Haixiang <haixiang6123@gmail.com>
2018-10-13 18:00:48 -0700

    Add: meta data

--- src/App.vue
+++ src/App.vue
@@ -36,79 +36,13 @@
 </template>
 
 <script>
+    import db from './db/meta'
+    const {nav, menu} = db;
     export default {
         data() {
             return {
-                nav: [
-                    {
-                        id: 1,
-                        link: '/',
-                        name: 'Home'
-                    },
-                    {
-                        id: 2,
-                        link: '/products',
-                        name: 'Products'
-                    },
-                    {
-                        id: 3,
-                        link: '/about',
-                        name: 'About'
-                    }
-                ],
-                menu: [
-                    {
-                        id: 1,
-                        icon: 'user',
-                        name: 'SubMenu 1',
-                        menuItems: [
-                            {
-                                id: 11,
-                                name: 'Home 1',
-                                link: '/'
-                            },
-                            {
-                                id: 12,
-                                name: 'Home 2',
-                                link: '/'
-                            },
-                        ]
-                    },
-                    {
-                        id: 2,
-                        icon: 'user',
-                        name: 'SubMenu 2',
-                        menuItems: [
-                            {
-                                id: 21,
-                                name: 'Products 1',
-                                link: '/products'
-                            },
-                            {
-                                id: 22,
-                                name: 'Products 2',
-                                link: '/products'
-                            },
-                        ]
-                    },
-                    {
-                        id: 3,
-                        icon: 'user',
-                        name: 'SubMenu 3',
-                        menuItems: [
-                            {
-                                id: 31,
-                                name: 'About 1',
-                                link: '/about'
-                            },
-                            {
-                                id: 32,
-                                name: 'About 2',
-                                link: '/about'
-                            },
-                        ]
-                    }
-                ],
+                nav,
+                menu
             }
         },
     }
