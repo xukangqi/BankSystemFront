74c6d185fd588236de82b55f9cb42ba46837f138
Haixiang <haixiang6123@gmail.com>
2018-10-15 23:19:38 -0700

    Add: auth function

--- src/routes/router.js
+++ src/routes/router.js
@@ -5,7 +5,7 @@ import Login from '../views/login/Login.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
     mode: 'history',
     linkActiveClass: 'active',
     routes: [
@@ -17,6 +17,7 @@ export default new Router({
         {
             path: '/admin/user',
             component: Admin,
+            meta: {requiresAuth: true},
             children: [
                 {
                     path: '',
@@ -94,3 +95,29 @@ export default new Router({
         },
     ]
 })
+
+const auth = {
+    loggedIn() {
+        return true
+    }
+}
+
+router.beforeEach((to, from, next) => {
+
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        // this route requires auth, check if logged in
+        // if not, redirect to login page.
+        if (!auth.loggedIn()) {
+            next({
+                path: '/',
+                query: { redirect: to.fullPath }
+            })
+        } else {
+            next()
+        }
+    } else {
+        next() // 确保一定要调用 next()
+    }
+})
+
+export default router
