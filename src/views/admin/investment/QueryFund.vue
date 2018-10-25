3632c34af2cd6a6b0a30fa345118f4ff50315031
徐杨杨 <742046560@qq.com>
2018-10-25 13:44:24 +0800

    基金查询业务功能完善

--- src/views/admin/investment/QueryFund.vue
+++ src/views/admin/investment/QueryFund.vue
@@ -30,23 +30,115 @@
   </a-table>
 
   <a-drawer
-      width=400
+      width=440
       placement="right"
       :closable="false"
       @close="onClose"
       :visible="visible"
       style="height: calc(100% - 55px);overflow: 'auto';paddingBottom: 53px;"
     >
-      <div style="background:#ECECEC; padding:30px;">
-  		<a-card title="基金产品详情" :bordered="false" style="width: 300px" hoverable>
-	    	<p>基金代码：{{detailValue.fundId}}</p>
-	    	<p>交易类型：{{detailValue.type}}</p>
-	    	<p>申购费率：{{detailValue.purchaseRate}}%</p>
-	    	<p>基金单位净值：{{detailValue.netAssetValue}}</p>
-	    	<p>赎回费率：{{detailValue.redemptionRate}}%</p>
-	    	<p>记录时间：{{detailValue.recordDate}}</p>
-  		</a-card>
-	  </div>
+      
+
+      <a-collapse defaultActiveKey="1" class="customPanelStyle">
+        <a-collapse-panel header="基金产品详情" key="1">
+          <div style="background:#ECECEC; padding:30px;">
+            <a-card :bordered="false" style="width: 300px" hoverable>
+              <p>基金代码：{{detailValue.fundId}}</p>
+              <p>交易类型：{{detailValue.type}}</p>
+              <p>申购费率：{{detailValue.purchaseRate}}%</p>
+              <p>基金单位净值：{{detailValue.netAssetValue}}</p>
+              <p>赎回费率：{{detailValue.redemptionRate}}%</p>
+              <p>记录时间：{{detailValue.recordDate}}</p>
+            </a-card>
+           </div>
+        </a-collapse-panel>
+        <a-collapse-panel header="基金购买申请" key="2">
+          <div style="background:#ECECEC; padding:30px;">
+            <a-card :bordered="false" style="width: 300px" hoverable>
+              <a-form layout="vertical" :autoFormCreate="(form)=>{this.form = form}">
+                <a-row>
+                  <a-col :span="3"/>
+                  <a-col :span="18">
+                    <a-form-item label='用户名' fieldDecoratorId="name"
+        :fieldDecoratorOptions="{rules: [{ required: true, message: '用户名不能为空' }]}">
+                       <a-input placeholder="请输入用户名"/>
+                    </a-form-item>
+                  </a-col>
+                  <a-col :span="3"/>
+                </a-row>
+                <a-row>
+                  <a-col :span="3"/>
+                  <a-col :span="18">
+                    <a-form-item label='用户手机号' fieldDecoratorId="phone"
+        :fieldDecoratorOptions="{rules: [{required: true, message: '请输入用户手机号' },{validator: this.checkTelephone}]}">
+                       <a-input placeholder="请输入用户手机号" />
+                    </a-form-item>
+                  </a-col>
+                  <a-col :span="3"/>
+                </a-row>
+                <a-row>
+                  <a-col :span="3"/>
+                  <a-col :span="18">
+                    <a-form-item label="账户号" fieldDecoratorId="account" :fieldDecoratorOptions="{ rules: [{ required: true, message: '账户号不能为空' }]}">
+                      <a-input placeholder="请输入账户号"/>
+                    </a-form-item>
+                  </a-col>
+                  <a-col :span="3"/>
+                </a-row>
+                <a-row>
+                  <a-col :span="3"/>
+                  <a-col :span="18">
+                    <a-form-item label="交易类型" fieldDecoratorId="type">
+                      <a-col :span="8" :offset="8">{{this.tradeType}}</a-col>
+                    </a-form-item>
+                  </a-col>
+                  <a-col :span="3"/>
+                </a-row>
+                <a-row>
+                  <a-col :span="3"/>
+                  <a-col :span="18">
+                    <a-form-item label="投资金额" fieldDecoratorId="amount":fieldDecoratorOptions="{ rules: [{ required: true, message: '投资金额不能为空' }]}">
+                      <a-input placeholder="请输入投资金额" />
+                    </a-form-item>
+                  </a-col>
+                  <a-col :span="3"/>
+                </a-row>
+                <a-row>
+                  <a-col :span="3"/>
+                  <a-col :span="18">
+                    <a-form-item label="账户密码" fieldDecoratorId="password" :fieldDecoratorOptions="{ rules: [{ required: true, message: '密码不能为空' }]}">
+                      <a-input placeholder="请输入账户密码" v-model = "pwd" type="password" />
+                    </a-form-item>
+                  </a-col>
+                  <a-col :span="3"/>
+                </a-row>
+                <a-row>
+                  <a-col :span="3"/>
+                  <a-col :span="18">
+                    <a-form-item label="重新输入密码" fieldDecoratorId="confirmpwd" :fieldDecoratorOptions="{ rules: [{ required: true, message: '密码不能为空' },{validator: this.checkPassword}]}">
+                      <a-input placeholder="请再次输入账户密码" type="password"/>
+                    </a-form-item>
+                  </a-col>
+                  <a-col :span="3"/>
+                </a-row>
+                <a-row>
+                  <a-col :span="6"/>
+                  <a-col :span="15">
+                    <a-form-item>
+                      <a-button-group>
+                        <a-button type='primary' htmlType='submit' @click="handleSubmit">提交</a-button>
+                        <a-button type='danger' @click="handleFormReset">重置</a-button>
+                      </a-button-group>
+                    </a-form-item>
+                  </a-col>
+                  <a-col :span="3"/>
+                </a-row>
+              </a-form>
+            </a-card>
+           </div>
+        </a-collapse-panel>
+      </a-collapse>
+
       <div
         :style="{
           position: 'absolute',
@@ -86,7 +178,7 @@ export default {
                     if (status == 200) {
                     	this.loading = false;
                     	for(let i=0; i<result.data.length; i++) {
-                    		result.data[i].type = (result.data[i].type==0?"认购/申购":"赎回");
+                    		result.data[i].type = (result.data[i].type==0?"认购":"申购");
                     		result.data[i].purchaseRate = result.data[i].purchaseRate.toFixed(2);
                     		result.data[i].netAssetValue = result.data[i].netAssetValue.toFixed(4);
                     		result.data[i].redemptionRate = result.data[i].redemptionRate.toFixed(2);
@@ -116,6 +208,8 @@ export default {
       visible: false,
       searchText: '',
       detailValue: {},
+      tradeType: '',
+      pwd: undefined,
       columns: [
       	{
         title: '基金代码',
@@ -140,12 +234,12 @@ export default {
         dataIndex: 'type',
         key: 'type',
         filters: [{
-            text: "认购/申购",
-            value: "认购/申购"
+            text: "认购",
+            value: "认购"
             },
             {
-            	text: "赎回",
-            	value: "赎回"
+            	text: "申购",
+            	value: "申购"
             }
             ],
             onFilter: (value, record) => record.type == value
@@ -184,6 +278,7 @@ export default {
     showDrawer(value) {
       this.visible = true;
       this.detailValue = this.showDetail(value);
+      this.tradeType = this.detailValue.type;
     },
     onClose() {
       this.visible = false
@@ -201,8 +296,41 @@ export default {
     formatDate(value) {
                 let date = new Date(parseInt(value) / 1000);
                 return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
-            }
+    },
+    handleSubmit(e) {
+              e.preventDefault();
+              this.form.validateFields((err, values) => {
+                if (!err) {
+                  values.type = this.tradeType;
+                  console.log("Received values of form: ", values);
+                }
+              });
+    },
+    handleFormReset() {
+      this.form.resetFields();
+    },
+    checkTelephone(rule, value, callback) {
+                if (value.length == 0) {
+                  callback();
+                }
+                if (!/^1[34578]\d{9}$/.test(value)) {
+                  callback("手机号码错误");
+                } else {
+                  callback();
+                }
+    },
+    checkPassword(rule, value, callback) {
+        if (typeof (value) == undefined) {
+          callback('请再次输入密码');
+        } else {
+          if (value === this.pwd) {
+            callback();
+          } else {
+            callback('密码不一致');
+          }
+        }
   },
+}
 }
 </script>
 <style scoped>
@@ -224,5 +352,13 @@ export default {
 
 .highlight {
   color: #f50;
+}
+
+.customPanelStyle {
+  background: #f7f7f7;
+  borderRadius: 50;
+  marginBottom: 24;
+  border: 0;
+  overflow: hidden;
 }
 </style>
